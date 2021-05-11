ff4550558b4a438a17a1091f7eab35463e9fb0ef
michalcik10 <76246984+michalcik10@users.noreply.github.com>
2021-05-11 23:33:01 +0200

    Navigation
    Dokoncena navigacia pre ucitela treba doplnit este tabulku

--- web/public/teacher/nav_Teacher.php
+++ web/public/teacher/nav_Teacher.php
@@ -1,16 +1,36 @@
 <?php
+include "../../app/vendor/autoload.php";
+use App\Model\LogsModel;
+
+//error_reporting(0);
+$contr = new LogsModel();
+
+session_start();
+
+$user = $contr->selectTeacher($_SESSION['email']);
 
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 
-</body>
-</html>
+<div uk-sticky="animation: uk-animation-slide-top; sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; cls-inactive: uk-navbar-transparent uk-light; top: 200" >
+    <nav class="uk-navbar-container" uk-navbar="dropbar: true;" style="position: relative; z-index: 980;">
+        <div class="uk-navbar-left">
+
+            <ul class="uk-navbar-nav">
+                <li><a href="?token=" class="uk-button">Generate Exam Code</a></li>
+                <li><a type="button" class="uk-button" data-toggle="modal" data-target="#myModal2">Create Test</a></li>
+                <li><a type="button" class="uk-button" data-toggle="modal" data-target="#myModal">Create Question</a></li>
+                <li><a href="#" type="button" class="uk-button" >Table of Students</a></li>
+            </ul>
+
+        </div>
+        <div class="uk-navbar-right">
+
+            <ul class="uk-navbar-nav">
+                <li><a><i uk-icon="users"></i></a></li>
+                <li><a><p><?php echo "<i>".$user[0]['name']." ".$user[0]['surname']."</i>";?><br><?php echo $user[0]['email'];?></p></a></li>
+                <li><a href="../logs/Logout.php" type="button" class="uk-button" >Logout</a></li>
+            </ul>
+
+        </div>
+    </nav>
+</div>
