94b7d169ccc2d6f38657f948f43b3d03a64e9455
luxao <74666305+luxao@users.noreply.github.com>
2021-05-14 18:25:06 +0200

    update language

--- web/public/index.php
+++ web/public/index.php
@@ -15,7 +15,7 @@ $controller = new Controller();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Documentation</title>
+    <title>Dokumentácia</title>
     <link rel="stylesheet" href="assets/css/style.css">
 
 
